66ec0fca06c01c98cb5f2eb8ef102cbed80075d3
rheahemmadi <rheahemmadi@gmail.com>
2023-09-02 14:10:50 +0100

    removed console logs

--- src/app.js
+++ src/app.js
@@ -65,10 +65,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "e6c2364656962bdcb16bc352fc42569a";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 function displayTemperature(response) {
